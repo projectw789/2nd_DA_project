d3ee762e21636f3be7c900802061975490808906
projectw789 <projectw789@gmail.com>
2026-09-09 14:31:52 +0100

    PYTEST WORKS. going to add more pytests for other individual functions too.

--- tests/score_testing.py
+++ tests/score_testing.py
@@ -1,8 +1,18 @@
-from ..src.job_application_analysis.scoring import calculate_score
-
-score = calculate_score (2,2,True,False)
-assert score == 80
+from job_application_analysis.scoring import calculate_score
 
 
+def test_score():
+    score = calculate_score(["1","2"],["1","2"],True,False)
+    assert score == 80
+
+
+def test_score_two():
+    score = calculate_score(["1","2"],[],False,False)
+    assert score == 0
+
+
+def test_score_three():
+    score = calculate_score(["1","2"],["1","2"],False,True)
+    assert score == 70
 
 # uv run pytest tests/score_testing.py
